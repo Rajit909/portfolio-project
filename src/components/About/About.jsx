f1de05b4810b3e9a19fbab0312fa704b7b24e08a
Rajit kumar <krajit909@gmail.com>
2023-08-22 21:55:55 +0530

    building about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import Photo from "../../assets/dpm.png";
+import Button from "../Button/Button";
+import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { ImBlog } from "react-icons/im";
+import 'animate.css'
+
 
 function About() {
   return (
-    <div>
+    <>
       
-    </div>
+    </>   
   )
 }
 
